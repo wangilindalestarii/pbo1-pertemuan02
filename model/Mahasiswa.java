093896d83b0fc7021ff27755a55dd62cb945635d
wangilindalestarii <wangilindalestari23@gmail.com>
2021-04-03 12:06:33 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+ System.out.println();
+ System.out.println("== Data Mahasiswa ======================");
+ System.out.println("NPM  : 19631063");
+ System.out.println("Nama : Wangi Linda Lestari");
+ System.out.println("====================================");
+ }
+}
